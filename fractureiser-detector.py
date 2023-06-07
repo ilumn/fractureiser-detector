1eb0b303925d9efaabfa61ec9b21c250d889fd8d
illumination <42507764+ilumn@users.noreply.github.com>
2023-06-07 04:30:17 -0400

    Update fractureiser-detector.py

--- fractureiser-detector.py
+++ fractureiser-detector.py
@@ -1,28 +1,72 @@
+import shutil
 import os
 import subprocess
 
-def search_in_file(file_path, search_string):
-    with open(file_path, 'r') as file:
-        for line_number, line in enumerate(file, 1):
-            if search_string in line:
-                print(f'Warning: File {file_path} is infected!')
+PROCYON_JAR_PATH = "procyon-decompiler.jar"
 
-def search_in_directory(directory, search_string):
-    for root, dirs, files in os.walk(directory):
+
+def install_procyon():
+    procyon_url = "https://github.com/mstrobel/procyon/releases/download/v0.6.0/procyon-decompiler-0.6.0.jar"
+    subprocess.run(["curl", "-L", "-o", PROCYON_JAR_PATH, procyon_url], check=True)
+
+
+def decompile_jar(jar_file, output_dir):
+    subprocess.run(["java", "-jar", PROCYON_JAR_PATH, "-o", output_dir, jar_file], check=True)
+
+
+def search_in_directory(directory, search_strings):
+    is_infected = False
+    for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith('.java'):
+            if file.endswith(".java"):
                 file_path = os.path.join(root, file)
-                search_in_file(file_path, search_string)
+                is_infected |= search_in_file(file_path, search_strings)
+    return is_infected
+
+
+def search_in_file(file_path, search_strings):
+    is_infected = False
+    with open(file_path, "r") as file:
+        for line in file:
+            for search_string in search_strings:
+                if search_string in line:
+                    is_infected = True
+                    print(f"Warning: File {file_path} is infected!")
+                    break
+    return is_infected
+
 
 def main():
-    jar_file_path = input('Enter the path to the JAR file: ')
-    search_string = '85.217.144.130'
+    jar_file = input("Enter the path to the JAR file: ")
+    search_strings = [
+        "85.217.144.130",
+        "56, 53, 46, 50, 49, 55, 46, 49, 52, 52, 46, 49, 51, 48",
+        "-74.-10.78.-106.12",
+        "114.-18.38.108.-100",
+        "-114.-18.38.108.-100"
+    ]
+    is_infected = False
 
-    # Extract the contents of the JAR file
-    subprocess.run(['jar', 'xf', jar_file_path])
+    # Check if Procyon decompiler is installed
+    if not os.path.exists(PROCYON_JAR_PATH):
+        install_procyon()
 
-    # Recursively search for the string in all extracted files
-    search_in_directory('.', search_string)
+    # Create a temporary directory for decompiled files
+    decompiled_dir = os.path.join(os.getcwd(), "decompiled")
+    os.makedirs(decompiled_dir, exist_ok=True)
 
-if __name__ == '__main__':
+    # Decompile the JAR file using Procyon decompiler
+    decompile_jar(jar_file, decompiled_dir)
+
+    # Recursively search for the strings in the decompiled files
+    is_infected = search_in_directory(decompiled_dir, search_strings)
+
+    # Cleanup: Delete the temporary directory
+    shutil.rmtree(decompiled_dir)
+
+    if is_infected:
+        print("Warning: The JAR file contains infected files.")
+
+
+if __name__ == "__main__":
     main()

c7c2d16f49d0300016af892e0cc8d1c258bb3608
illumination <42507764+ilumn@users.noreply.github.com>
2023-06-07 04:53:21 -0400

    Update fractureiser-detector.py
    add directory search

--- fractureiser-detector.py
+++ fractureiser-detector.py
@@ -1,6 +1,7 @@
-import shutil
 import os
+import argparse
 import subprocess
+import shutil
 
 PROCYON_JAR_PATH = "procyon-decompiler.jar"
 
@@ -18,55 +19,84 @@ def search_in_directory(directory, search_strings):
     is_infected = False
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith(".java"):
+            if file.endswith(".jar"):
                 file_path = os.path.join(root, file)
-                is_infected |= search_in_file(file_path, search_strings)
+                is_infected |= analyze_jar(file_path, search_strings)
     return is_infected
 
 
-def search_in_file(file_path, search_strings):
+def analyze_jar(jar_file, search_strings):
     is_infected = False
-    with open(file_path, "r") as file:
-        for line in file:
-            for search_string in search_strings:
-                if search_string in line:
-                    is_infected = True
-                    print(f"Warning: File {file_path} is infected!")
-                    break
-    return is_infected
-
-
-def main():
-    jar_file = input("Enter the path to the JAR file: ")
-    search_strings = [
-        "85.217.144.130",
-        "56, 53, 46, 50, 49, 55, 46, 49, 52, 52, 46, 49, 51, 48",
-        "-74.-10.78.-106.12",
-        "114.-18.38.108.-100",
-        "-114.-18.38.108.-100"
-    ]
-    is_infected = False
-
-    # Check if Procyon decompiler is installed
-    if not os.path.exists(PROCYON_JAR_PATH):
-        install_procyon()
 
     # Create a temporary directory for decompiled files
     decompiled_dir = os.path.join(os.getcwd(), "decompiled")
     os.makedirs(decompiled_dir, exist_ok=True)
 
-    # Decompile the JAR file using Procyon decompiler
-    decompile_jar(jar_file, decompiled_dir)
+    try:
+        # Decompile the JAR file using Procyon decompiler
+        decompile_jar(jar_file, decompiled_dir)
 
-    # Recursively search for the strings in the decompiled files
-    is_infected = search_in_directory(decompiled_dir, search_strings)
-
-    # Cleanup: Delete the temporary directory
-    shutil.rmtree(decompiled_dir)
+        # Recursively search for the strings in the decompiled files
+        is_infected = search_in_directory(decompiled_dir, search_strings)
+    finally:
+        # Cleanup: Delete the temporary directory
+        shutil.rmtree(decompiled_dir)
 
     if is_infected:
-        print("Warning: The JAR file contains infected files.")
+        print(f"Warning: The JAR file '{jar_file}' contains infected files.")
+
+    return is_infected
+
+
+def scan_jar(jar_file, search_strings):
+    is_infected = analyze_jar(jar_file, search_strings)
+
+    if not is_infected:
+        print("No infected files found in the JAR file.")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="JAR File Scanner")
+    parser.add_argument("file", help="JAR file to scan")
+    parser.add_argument(
+        "--recursive",
+        action="store_true",
+        help="Scan JAR files in the directory recursively",
+    )
+
+    args = parser.parse_args()
+    jar_path = args.file
+    recursive_mode = args.recursive
+
+    search_strings = [
+        "85.217.144.130",
+        "56, 53, 46, 50, 49, 55, 46, 49, 52, 52, 46, 49, 51, 48",
+        "-74.-10.78.-106.12",
+        "114.-18.38.108.-100",
+        "-114.-18.38.108.-100",
+    ]
+
+    # Check if Procyon decompiler is installed
+    if not os.path.exists(PROCYON_JAR_PATH):
+        install_procyon()
+
+    if recursive_mode:
+        if not os.path.isdir(jar_path):
+            print("Error: The specified path is not a directory.")
+            return
+
+        is_infected = search_in_directory(jar_path, search_strings)
+
+        if not is_infected:
+            print("No infected files found in the JAR files.")
+    else:
+        if not os.path.isfile(jar_path):
+            print("Error: The specified path is not a file.")
+            return
+
+        scan_jar(jar_path, search_strings)
 
 
 if __name__ == "__main__":
     main()
+    input("press any key to continue")
